0884a7d5b21d2ff6b826fe491a8813c59d5325c7
Griffin Moore <griffinmoore@gmail.com>
2013-02-17 20:30:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zeitgeist::Application.config.secret_token = '1b48c65e091a6fd1db3cfc70392f65c7615a67b363f9cb329e36058d6a4b8b75230f9567f367d66a7d55c4b435a9c9e8f51ad409941be44711496734b6c05190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zeitgeist::Application.config.session_store :cookie_store, key: '_zeitgeist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zeitgeist::Application.config.session_store :active_record_store
